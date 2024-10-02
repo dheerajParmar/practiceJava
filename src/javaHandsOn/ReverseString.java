a0fc23e7512b6f84f099b063d057c117090c12e2
dheeraj <dheerajparmar014@gmail.com>
2024-10-02 22:31:31 +0530

    Reverse String Code

---
+++ src/javaHandsOn/ReverseString.java
@@ -0,0 +1,18 @@
+package javaHandsOn;
+
+public class ReverseString {
+	static String result = "";
+	public static void main(String[] args) {
+		String s1 = "Dheeraj Parmar";
+		reverseString(s1);
+		System.out.println(result);
+	}
+	
+	public static String reverseString(String input) {
+		for(int i=input.length()-1; i >= 0; i--) {
+			result += input.charAt(i);
+		}
+		return result;
+	}
+
+}
